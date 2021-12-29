e1ce04e856e3a098b4b920758c46ed8daf17d6ba
hteta <htetaung200071@gmail.com>
2021-12-30 01:21:13 +0630

    initial commit

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var cors = require('cors');
+const multer  = require('multer')
+
 require('dotenv').config()
 
 var app = express();
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -12,6 +15,17 @@ app.get('/', function (req, res) {
 });
 
 
+var upload=multer({dest:"uploads/"});
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>
+    res.json(
+      {
+        name:req.file.originalname,
+        type:req.file.mimetype,
+        size:req.file.size
+    
+      }
+    )
+)
 
 
 const port = process.env.PORT || 3000;
